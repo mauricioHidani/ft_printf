a240d94ea453195d8255424af70598db519ad7d8
mauricioHidani <mhidani@student.42org.br>
2025-08-12 09:22:31 -0300

    refactor: remove file descriptor attrb

--- srcs/ft_putnbr.c
+++ srcs/ft_putnbr.c
@@ -6,13 +6,13 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 22:26:33 by mhidani           #+#    #+#             */
-/*   Updated: 2025/08/08 22:33:13 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/08/12 08:46:00 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr(const long nbr, int base, const char *digits, int fd)
+int	ft_putnbr(const long nbr, int base, const char *digits)
 {
 	int		count;
 	char	digit;
@@ -22,12 +22,12 @@ int	ft_putnbr(const long nbr, int base, const char *digits, int fd)
 	n = nbr;
 	if (n < 0)
 	{
-		count += write(fd, "-", 1);
+		count += write(1, "-", 1);
 		n = -n;
 	}
 	if (n >= (long)base)
-		count += ft_putnbr(n / base, base, digits, fd);
+		count += ft_putnbr(n / base, base, digits);
 	digit = digits[n % base];
-	count += write(fd, &digit, 1);
+	count += write(1, &digit, 1);
 	return (count);
 }
--- srcs/ft_putnbr_ul.c
+++ srcs/ft_putnbr_ul.c
@@ -6,13 +6,13 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 22:16:29 by mhidani           #+#    #+#             */
-/*   Updated: 2025/08/08 22:38:42 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/08/12 08:29:20 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putnbr_ul(const unsigned long nbr, int base, const char *digits, int fd)
+int	ft_putnbr_ul(const unsigned long nbr, int base, const char *digits)
 {
 	int				count;
 	char			digit;
@@ -22,8 +22,8 @@ int	ft_putnbr_ul(const unsigned long nbr, int base, const char *digits, int fd)
 	digit = 0;
 	n = nbr;
 	if (n >= (unsigned long)base)
-		count += ft_putnbr_ul(n / base, base, digits, fd);
+		count += ft_putnbr_ul(n / base, base, digits);
 	digit = digits[n % base];
-	count += write(fd, &digit, 1);
+	count += write(1, &digit, 1);
 	return (count);
 }
