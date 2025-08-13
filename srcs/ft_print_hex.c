87213364c4da62662cf498d81822ab6de231cdfe
Mauricio Mityo Hidani <mhidani@c1r4p4.42sp.org.br>
2025-08-13 10:56:54 -0300

    refactor: prototypies

--- srcs/ft_print_hex.c
+++ srcs/ft_print_hex.c
@@ -6,16 +6,18 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 22:55:37 by mhidani           #+#    #+#             */
-/*   Updated: 2025/08/08 23:01:39 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/08/12 08:35:03 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_hex(const unsigned int nbr)
+int	ft_print_hex(va_list ap)
 {
-	int	count;
+	int				count;
+	unsigned int	nbr;
 
-	count = ft_putnbr_ul((unsigned long)nbr, 16, HEX_LOW, 1);
+	nbr = va_arg(ap, unsigned int);
+	count = ft_putnbr_ul(nbr, 16, "0123456789abcdef");
 	return (count);
 }
--- srcs/ft_print_perc.c
+++ srcs/ft_print_perc.c
@@ -1,30 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_str.c                                     :+:      :+:    :+:   */
+/*   ft_print_perc.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/08 21:43:32 by mhidani           #+#    #+#             */
-/*   Updated: 2025/08/12 08:27:33 by mhidani          ###   ########.fr       */
+/*   Created: 2025/08/12 09:01:49 by mhidani           #+#    #+#             */
+/*   Updated: 2025/08/12 09:06:54 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_print_str(va_list ap)
+int	ft_print_perc(va_list ap)
 {
-	int		count;
-	char	*str;
-
-	str = va_arg(ap, char *);
-	if (!str)
-	{
-		count = write(1, "(null)", 6);
-		return (count);
-	}
-	count = 0;
-	while (*str)
-		count += write(1, str++, 1);
-	return (count);
+	(void)ap;
+	write(1, "%", 1);
+	return (1);
 }
