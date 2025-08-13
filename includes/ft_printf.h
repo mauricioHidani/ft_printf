3f4b13e6d8bb985a8f03217bac0c50ab5f7bed35
Mauricio Mityo Hidani <mhidani@c1r4p4.42sp.org.br>
2025-08-13 10:55:06 -0300

    refactor: lib

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 11:15:16 by mhidani           #+#    #+#             */
-/*   Updated: 2025/08/11 18:08:43 by mhidani          ###   ########.fr       */
+/*   Updated: 2025/08/12 09:03:06 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,24 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-# define DEC "0123456789"
-# define HEX_LOW "0123456789abcdef"
-# define HEX_UP "0123456789ABCDEF"
+typedef struct s_specifier
+{
+	char	type;
+	int		(*f)(va_list);
+}	t_specifier;
 
 int	ft_printf(const char *str, ...);
 
-int	ft_print_char(const char c);
-int	ft_print_str(const char *str);
-int	ft_print_addr(void *ptr);
-int	ft_print_dec(const int nbr);
-int	ft_print_udec(const unsigned int nbr);
-int	ft_print_hex(const unsigned int nbr);
-int	ft_print_hexup(const unsigned int nbr);
+int	ft_print_char(va_list ap);
+int	ft_print_str(va_list ap);
+int	ft_print_addr(va_list ap);
+int	ft_print_dec(va_list ap);
+int	ft_print_udec(va_list ap);
+int	ft_print_hex(va_list ap);
+int	ft_print_hexup(va_list ap);
+int	ft_print_perc(va_list ap);
 
-int	ft_putnbr(const long nbr, int base, const char *digits, int fd);
-int	ft_putnbr_ul(const unsigned long nbr, int base, const char *digits, int fd);
+int	ft_putnbr(const long nbr, int base, const char *digits);
+int	ft_putnbr_ul(const unsigned long nbr, int base, const char *digits);
 
 #endif
